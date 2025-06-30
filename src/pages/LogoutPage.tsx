f8a4da54b9f3c94b8743481884a2d8ffa613d73b
sorrynogmailauthentication <mike.volchek@gmail.com>
2025-06-30 18:46:27 +0300

    logout-centered

--- src/pages/LogoutPage.tsx
+++ src/pages/LogoutPage.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@chakra-ui/react'
+import { Button, HStack } from '@chakra-ui/react'
 import { useAuthData } from '../state-management/store'
 import apiClient from '../services/ApiClientJsonServer';
 import { useNavigate } from 'react-router-dom';
@@ -8,7 +8,9 @@ const LogoutPage = () => {
     const navigate = useNavigate();
     apiClient.setToken("");
   return (
+    <HStack justifyContent={"center"} p={"10vh"}>
     <Button onClick={() => {logout(); navigate("/login")}}>Logout</Button>
+    </HStack>
   )
 }
 
